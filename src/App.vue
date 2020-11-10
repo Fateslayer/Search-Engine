b12754f4639b8bada23b8f0a9e9e6d1cec439aee
Ajaz Ur Rehman <fateslayer47@gmail.com>
2020-11-10 13:09:07 +0530

    Add font and set global styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
 	<router-view />
 </template>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+body {
+	margin: 0;
+	font-family: 'Roboto', sans-serif;
+}
+</style>
