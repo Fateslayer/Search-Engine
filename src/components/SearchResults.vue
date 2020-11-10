81276ebeafe271260e2bd13d0e0b044bf99c8114
Ajaz Ur Rehman <fateslayer47@gmail.com>
2020-11-10 17:47:10 +0530

    Create search results component

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="results">
+		<template v-if="results.items.length">
+			<p>Results Found</p>
+		</template>
+		<p class="text-center lead" v-else>
+			Your search -
+			<span class="font-weight-bold">{{ results.query }}</span> did not
+			match any documents.
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		results: {
+			type: Object,
+			required: true,
+		},
+	},
+};
+</script>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,21 +1,33 @@
 <template>
-	<div class="container py-3">
+	<div class="container py-4">
 		<search-input :value="query" @search="search"></search-input>
+		<hr class="my-4" />
+		<search-results :results="results"></search-results>
 	</div>
 </template>
 
 <script>
 import SearchInput from '../components/SearchInput';
+import SearchResults from '../components/SearchResults';
 
 export default {
 	components: {
 		SearchInput,
+		SearchResults,
 	},
 	inject: ['search'],
 	data() {
 		return {
 			query: '',
 		};
+	},
+	computed: {
+		results() {
+			return {
+				items: [],
+				query: this.query,
+			};
+		},
 	},
 	methods: {
 		setQuery() {
