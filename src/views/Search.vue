5c53bb7bdb9a95020bac500ad53689b5d3b78265
Ajaz Ur Rehman <fateslayer47@gmail.com>
2020-11-28 16:18:29 +0530

    Add query string based pagination

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -23,6 +23,8 @@ export default {
 	data() {
 		return {
 			query: '',
+			page: 1,
+			limit: 10,
 			resultData: {},
 			searching: false,
 		};
@@ -30,12 +32,14 @@ export default {
 	methods: {
 		setQuery() {
 			this.query = this.$route.query.q;
+			this.page = Math.abs(+this.$route.query.page || 1);
+			this.limit = Math.abs(+this.$route.query.limit || 10);
 			this.getResults();
 		},
 		async getResults() {
 			this.searching = true;
 			this.resultData = {};
-			const url = `http://localhost:3000/search?q=${this.query}`;
+			const url = `http://localhost:3000/search?q=${this.query}&page=${this.page}&limit=${this.limit}`;
 			const response = await fetch(url);
 
 			if (response.ok) {
